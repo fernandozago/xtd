4fa3deeecd721d28571f4db0718ec3644114923b
Fernando Zago <fe.zago@live.com>
2026-07-17 15:58:36 -0300

    improved benchmarks results

--- tests/pipelines.sh
+++ tests/pipelines.sh
@@ -14,16 +14,15 @@ cd "$ROOT_DIR"
 mkdir -p "$BIN_DIR"
 
 rm -f "$TEST_BINARY"
-echo "Building Pipelines tests..."
-g++-15 @"$COMPILE_FLAGS_FILE" "$TEST_SOURCE" -o "$TEST_BINARY" -O3
-echo "Running Pipelines tests..."
-"$TEST_BINARY" #--duration=true --success=true "$@"
-echo "done!"
+echo "Building tests..."
+g++-15 "$TEST_SOURCE" -o "$TEST_BINARY" -O3 -pthread @"$COMPILE_FLAGS_FILE" 
+echo "Running tests..."
+"$TEST_BINARY" #--duration=true #--success=true "$@"
 
 # rm -f "$TEST_BINARY"
-# clang++ -std=c++20 -I"$INCLUDE_DIR" "$TEST_SOURCE" -o "$TEST_BINARY" -Wall -Wextra -O3
+# echo "Building tests..."
+# clang++ "$TEST_SOURCE" -o "$TEST_BINARY" -O3 -pthread @"$COMPILE_FLAGS_FILE"
 # echo "Running tests..."
-# "$TEST_BINARY" #--duration=true --success=true "$@"
-# echo "done!"
+# "$TEST_BINARY" #--duration=true #--success=true "$@"
 
 rm -f "$TEST_BINARY"
