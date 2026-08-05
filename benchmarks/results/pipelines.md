ea242d217fea042aeadffc684a06da37bbbbf20a
Fernando Zago <fe.zago@live.com>
2026-08-05 09:46:53 -0300

    improving fixed_buffer_pool

--- benchmarks/results/pipelines.md
+++ benchmarks/results/pipelines.md
@@ -12,16 +12,16 @@ Recommendations
 
 |              μs/MB |                MB/s |    err% |     total | xtd::pipeline throughput
 |--------------------:|--------------------:|--------:|----------:|:-------------------------
-|              284.84 |            3,510.73 |    1.7% |      6.76 | `1 KB writes`
-|              172.40 |            5,800.45 |    1.3% |      6.82 | `2 KB writes`
-|              115.28 |            8,674.74 |    1.5% |      6.87 | `4 KB writes`
-|               83.89 |           11,920.61 |    1.9% |      6.96 | `8 KB writes`
-|               81.12 |           12,328.12 |    1.1% |      6.92 | `16 KB writes`
+|              273.54 |            3,655.76 |    1.4% |      6.98 | `1 KB writes`
+|              155.29 |            6,439.44 |    2.6% |      6.85 | `2 KB writes`
+|              102.87 |            9,721.28 |    0.8% |      6.83 | `4 KB writes`
+|               83.99 |           11,906.03 |    0.7% |      7.00 | `8 KB writes`
+|               83.24 |           12,013.04 |    1.8% |      6.89 | `16 KB writes`
 
 |   Total Transferred | xtd::pipeline throughput 
 |--------------------:|:-------------------------
-|            23.49 GB | `1 KB writes`
-|            39.12 GB | `2 KB writes`
-|            58.67 GB | `4 KB writes`
-|            81.83 GB | `8 KB writes`
-|            84.75 GB | `16 KB writes`
+|            25.23 GB | `1 KB writes`
+|            43.43 GB | `2 KB writes`
+|            65.33 GB | `4 KB writes`
+|            82.16 GB | `8 KB writes`
+|            82.78 GB | `16 KB writes`
--- src/pipeline/fixed_buffer_pool.h
+++ src/pipeline/fixed_buffer_pool.h
@@ -1,10 +1,10 @@
 #ifndef PIPELINE_FIXED_BUFFER_POOL_H
 #define PIPELINE_FIXED_BUFFER_POOL_H
 
+#include <cassert>
 #include <cstddef>
 #include <memory>
 #include <vector>
-#include <cassert>
 
 namespace xtd
 {
@@ -12,60 +12,63 @@ namespace xtd
     class fixed_buffer_pool
     {
     private:
-        std::vector<std::unique_ptr<std::byte[]>> m_available_buffers{};
+        std::vector<std::byte*> m_available_buffers;
         const std::size_t m_buffer_size;
         const std::size_t m_max_pool_size;
-        
-        struct unique_ptr_releaser
+
+        struct buffer_releaser
         {
-            fixed_buffer_pool* pool = nullptr;
+            fixed_buffer_pool& pool;
+
             void operator()(std::byte* buffer) const noexcept
             {
-                assert(pool != nullptr);
-                pool->release(buffer);
+                assert(buffer != nullptr);
+
+                if (pool.m_available_buffers.size() < pool.m_max_pool_size) {
+                    pool.m_available_buffers.push_back(buffer);
+                }
+                else {
+                    delete[] buffer;
+                }
             }
         };
 
-        void release(std::byte* buffer) noexcept
-        {
-            std::unique_ptr<std::byte[]> buffer_ptr(buffer);
-            if (m_available_buffers.size() < m_max_pool_size) {
-                m_available_buffers.push_back(std::move(buffer_ptr));
-            }
-
-            // If the pool is full, `buffer_ptr` automatically frees the memory.
-        }
-
-        std::unique_ptr<std::byte[]> get_buffer_from_pool() noexcept
-        {
-            std::unique_ptr<std::byte[]> buffer = std::move(m_available_buffers.back());
-            m_available_buffers.pop_back();
-            return buffer;
-        }
     public:
-        using fixed_buffer_ptr = std::unique_ptr<std::byte[], unique_ptr_releaser>;
+        using fixed_buffer_ptr = std::unique_ptr<std::byte[], buffer_releaser>;
 
         fixed_buffer_pool(const fixed_buffer_pool&) = delete;
         fixed_buffer_pool& operator=(const fixed_buffer_pool&) = delete;
         fixed_buffer_pool(fixed_buffer_pool&&) = delete;
         fixed_buffer_pool& operator=(fixed_buffer_pool&&) = delete;
 
-        explicit fixed_buffer_pool(const std::size_t buffer_size, const std::size_t max_pool_size)
-            : m_buffer_size(buffer_size)
-            , m_max_pool_size(max_pool_size)
+        explicit fixed_buffer_pool(
+            const std::size_t buffer_size,
+            const std::size_t max_pool_size)
+            : m_buffer_size{buffer_size}
+            , m_max_pool_size{max_pool_size}
         {
+            assert(buffer_size > 0);
             m_available_buffers.reserve(max_pool_size);
+        }
+
+        ~fixed_buffer_pool()
+        {
+            for (std::byte* buffer : m_available_buffers) {
+                delete[] buffer;
+            }
         }
 
         [[nodiscard]]
         fixed_buffer_ptr get_buffer()
         {
             if (m_available_buffers.empty()) {
-                return fixed_buffer_ptr{std::make_unique_for_overwrite<std::byte[]>(m_buffer_size).release(), unique_ptr_releaser{this}};
+                return fixed_buffer_ptr{new std::byte[m_buffer_size], buffer_releaser{*this}};
             }
-            else {
-                return fixed_buffer_ptr{get_buffer_from_pool().release(), unique_ptr_releaser{this}};
-            }
+
+            std::byte* buffer = m_available_buffers.back();
+            m_available_buffers.pop_back();
+
+            return fixed_buffer_ptr{buffer, buffer_releaser{*this}};
         }
 
         [[nodiscard]]
@@ -79,7 +82,6 @@ namespace xtd
         {
             return m_available_buffers.size();
         }
-
     };
 
 } // namespace xtd
